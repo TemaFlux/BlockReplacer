20b81e8c3d5385f1a426b83e36aeb3cea6ca4cac
TemaFlux <67328970+TemaFlux@users.noreply.github.com>
2020-07-18 22:15:32 +0300

    fix strings

--- src/me/temaflex/blockreplacer/Cmd.java
+++ src/me/temaflex/blockreplacer/Cmd.java
@@ -6,25 +6,25 @@ import org.bukkit.command.CommandSender;
 
 public class Cmd
 implements CommandExecutor {
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (args.length >= 1) {
-			switch (args[0]) {
-				case "reload": {
-					if (sender.hasPermission("blockreplacer.reload")) {
-						Main.getI().reloadConfig();
-						Main.getI().loadConfig();
-						Utils.sendM(sender, Main.getI().getConfig().get("messages.reload"), cmd.getName());
-						return true;
-					}
-					else {
-						Utils.sendM(sender, Main.getI().getConfig().get("messages.noperm"), cmd.getName());
-						return true;
-					}
-				}
-			}
-		}
-		Utils.sendM(sender, Main.getI().getConfig().get("messages.help"), cmd.getName());
-		return false;
-	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length >= 1) {
+            switch (args[0]) {
+                case "reload": {
+                    if (sender.hasPermission("blockreplacer.reload")) {
+                        Main.getI().reloadConfig();
+                        Main.getI().loadConfig();
+                        Utils.sendM(sender, Main.getI().getConfig().get("messages.reload"), cmd.getName());
+                        return true;
+                    }
+                    else {
+                        Utils.sendM(sender, Main.getI().getConfig().get("messages.noperm"), cmd.getName());
+                        return true;
+                    }
+                }
+            }
+        }
+        Utils.sendM(sender, Main.getI().getConfig().get("messages.help"), cmd.getName());
+        return false;
+    }
 }
